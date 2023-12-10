c2fcb6f0e5efd3a6ba06c4d29a6cacd90706c0bf
mizonov-iv <mizonov.iv@gmail.com>
2023-12-10 16:37:14 +0500

    styling update

--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -1,54 +1,56 @@
 <template>
   <Navbar/>
-  <h2>Все заказы</h2>
-  <table v-if="eventsStore.allEvents.length">
-    <tr>
-      <td>ID</td>
-      <td>Имя</td>
-      <td class="td-sortable" @click="sortByAddress(eventsStore.allEvents)">
-        <p>Адресс</p>
-        <svg class="sort-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 16">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M9 5 5 1 1 5m0 6 4 4 4-4"/>
-        </svg>
-      </td>
-      <td class="td-sortable" @click="sortByDate(eventsStore.allEvents)">
-        <p>Дата</p>
-        <svg class="sort-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 16">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M9 5 5 1 1 5m0 6 4 4 4-4"/>
-        </svg>
-      </td>
-      <td>Статус</td>
-      <td>Комментарий</td>
-      <td>Управление</td>
-    </tr>
-    <tr
-        v-for="event in eventsStore.allEvents"
-        :key="event.id"
-        :class="{completed: event.status === 'Выполнен'}"
-    >
-      <td>{{ event.id }}</td>
-      <td>{{ event.name }}</td>
-      <td>{{ event.address }}</td>
-      <td>{{ event.date }}</td>
-      <td>{{ event.status }}</td>
-      <td>{{ event.comment }}</td>
-      <td>
-        <EventsBtns
-            v-if="usersStore.currentUser.role === 'ADMIN'"
-            :event="event"
-            @delete="openDeleteModal"
-        />
-      </td>
-    </tr>
+  <div class="events-wrapper">
+    <h2 class="orders-header">Все заказы</h2>
+    <table v-if="eventsStore.allEvents.length">
+      <tr class="table-head">
+        <td>ID</td>
+        <td>Имя</td>
+        <td class="td-sortable" @click="sortByAddress(eventsStore.allEvents)">
+          <p>Адресс</p>
+          <svg class="sort-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 16">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M9 5 5 1 1 5m0 6 4 4 4-4"/>
+          </svg>
+        </td>
+        <td class="td-sortable" @click="sortByDate(eventsStore.allEvents)">
+          <p>Дата</p>
+          <svg class="sort-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 16">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M9 5 5 1 1 5m0 6 4 4 4-4"/>
+          </svg>
+        </td>
+        <td>Статус</td>
+        <td>Комментарий</td>
+        <td>Управление</td>
+      </tr>
+      <tr
+          v-for="event in eventsStore.allEvents"
+          :key="event.id"
+          :class="{completed: event.status === 'Выполнен'}"
+      >
+        <td>{{ event.id }}</td>
+        <td>{{ event.name }}</td>
+        <td>{{ event.address }}</td>
+        <td>{{ event.date }}</td>
+        <td>{{ event.status }}</td>
+        <td>{{ event.comment }}</td>
+        <td>
+          <EventsBtns
+              v-if="usersStore.currentUser.role === 'ADMIN'"
+              :event="event"
+              @delete="openDeleteModal"
+          />
+        </td>
+      </tr>
+    </table>
+    <h4 v-else>Активные заказы отсутсвуют</h4>
 
-  </table>
-  <h4 v-else>Активные заказы отсутсвуют</h4>
+    <DeleteModal
+        v-if="modal.show.value === true"
+        @delete="handleDelete"
+        :eventID="eventToDeleteID"
+    />
+  </div>
 
-  <DeleteModal
-      v-if="modal.show.value === true"
-      @delete="handleDelete"
-      :eventID="eventToDeleteID"
-  />
 </template>
 
 <script lang="ts" setup>
